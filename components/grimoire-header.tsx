7c86695f7f4ee5c74a90a7341247b44657c9e77d
DrunkDez <43417938+DrunkDez@users.noreply.github.com>
2026-02-09 09:53:26 +0100

    Add files via upload

--- components/grimoire-header.tsx
+++ components/grimoire-header.tsx
@@ -1,8 +1,15 @@
 "use client"
 
+import { UserNav } from "@/components/auth/user-nav"
+
 export function GrimoireHeader() {
   return (
     <header className="relative bg-card px-6 py-12 text-center border-b-4 border-double border-primary">
+      {/* User Navigation - Top Right */}
+      <div className="absolute top-4 right-6 z-20">
+        <UserNav />
+      </div>
+
       {/* Top gold line */}
       <div
         className="absolute top-4 left-1/2 -translate-x-1/2 w-3/5 h-[3px]"
