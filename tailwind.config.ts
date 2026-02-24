c80c051d052d1434b2e71941615001ddc31cb32a
DrunkDez <43417938+DrunkDez@users.noreply.github.com>
2026-02-24 23:35:01 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,6 +93,9 @@ const config: Config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        animation: {
+  'spin-slow': 'spin 8s linear infinite',
+      }
       },
     },
   },
